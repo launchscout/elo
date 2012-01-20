9d2eee96dd8d14a042a5be27f060c936acf9de3f
Chris Moore <chrism@gaslightsoftware.com>
2012-01-20 16:22:15 -0500

    gravatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+require 'digest/md5'
+
 module ApplicationHelper
+  def gravatar_url_for(email, options = {})
+    options.reverse_merge!(:size => 40)
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{options[:size]}"
+  end
 end
