54a164d0d9dd4bebbfc30e63157762739a93f883
Doug Alcorn <dougalcorn@gmail.com>
2012-02-01 13:54:41 -0500

    order the players in the new games drop downs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_players
+
+  private
+
+  def load_players
+    @players ||= Player.all(:order => "rank DESC")
+  end
 end
