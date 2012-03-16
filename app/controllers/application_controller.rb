b103f12c7961a386072f91d0ec0ba43dd614a3c9
Peter Kananen <pkananen@gmail.com>
2012-03-15 23:21:53 -0400

    create a game for a given league

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  helper_method :active_league
   before_filter :load_players
 
   private
+
+  def active_league
+    League.first
+  end
 
   def load_players
     @players ||= Player.all(:order => "rank DESC")
